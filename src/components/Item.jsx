ef88227ae6ea90ae8ac8b193009870bf3bfef1cc
kiizerd <ajsewell13@gmail.com>
2022-10-25 15:24:47 -0500

    Styled Item component.
    Remove log statement.
    
    Ran prettier formatter.

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,28 +1,87 @@
 import { useState, useEffect } from "react";
-import { itemEndpoint } from "../endpoints";
+import ReactTimeAgo from "react-time-ago";
+import styled from "styled-components";
+import { itemEndpoint, userEndpoint } from "../endpoints";
 
-const Item = ({ id }) => {
+const ListItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  height: 3rem;
+  align-items: start;
+  margin: 5px;
+  padding: 5px;
+  background-color: #303030;
+  font-size: 12px;
+`;
+
+const Link = styled.a`
+  cursor: pointer;
+  color: rgb(254, 254, 254, 0.85);
+  font-size: ${(props) => (props.smallFont ? "12px" : "14px")};
+  font-style: ${(props) => (props.smallFont ? "italic" : "normal")};
+
+  &:visited {
+    color: rgb(248, 248, 248, 0.68);
+  }
+
+  &:hover {
+    color: rgb(255, 255, 255, 0.975);
+    text-decoration: underline;
+  }
+`;
+
+const ItemRow = styled.div`
+  display: flex;
+  gap: 5px;
+  margin-left: ${(props) => (props.leftMargin ? "16px" : "0")};
+`;
+
+const Item = ({ id, index }) => {
   const [data, setData] = useState({});
+  const [link, setLink] = useState("");
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem(`${id}`));
     if (storedData) {
-      console.log(storedData)
       setData(storedData);
     } else {
       getRemoteItem(id);
     }
   }, []);
 
+  // Store link separately to allow use of JS URl
+  useEffect(() => {
+    if (data.url) {
+      const itemURL = new URL(data.url);
+      setLink(itemURL);
+    }
+  }, [data]);
+
   async function getRemoteItem(itemId) {
     const itemResponse = await fetch(itemEndpoint(itemId));
     const item = await itemResponse.json();
 
     setData(item);
-    localStorage.setItem(`${id}`, JSON.stringify(item))
+    localStorage.setItem(`${id}`, JSON.stringify(item));
   }
 
-  return <li><div>{data.title}</div></li>;
+  return (
+    <ListItem>
+      <ItemRow>
+        <span>{index + 1}.</span>
+        <Link href={data.url}>{data.title}</Link>
+        <span>({link ? link.host : false})</span>
+      </ItemRow>
+      <ItemRow leftMargin>
+        <span>{data.score} points by</span>
+        <Link smallFont href={userEndpoint(data.by)}>
+          {data.by}
+        </Link>
+        <span>|</span>
+        <ReactTimeAgo date={new Date(data.time * 1000)} locale="en-US" />
+      </ItemRow>
+    </ListItem>
+  );
 };
 
 export default Item;
