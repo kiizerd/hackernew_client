aa23bfe9c6054523e13b621b5751ca1838886a92
kiizerd <ajsewell13@gmail.com>
2022-10-25 19:02:18 -0500

    Created a Header component to contain navigation links.

---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  padding: 4px;
+  display: flex;
+  gap: 12px;
+`;
+
+const PageTitle = styled.div`
+  font-size: 1.2rem;
+  margin-right: 8px;
+`;
+
+const HeaderList = styled.ul`
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  font-size: 0.8rem;
+
+  // Add a separator after every child but last
+  & > * {
+    &:after {
+      margin: 5px;
+      content: "|";
+    }
+    
+    &:last-child {
+      &:after {
+        content: "";
+      }
+    }
+  }
+`
+
+const Header = ({ tabLists }) => {
+  return (
+    <StyledHeader>
+      <PageTitle>HackerNews</PageTitle>
+      <HeaderList>
+        <li>best</li>
+        <li>new</li>
+        <li>comments</li>
+        <li>show</li>
+        <li>ask</li>
+        <li>jobs</li>
+        <li>read</li>
+      </HeaderList>
+    </StyledHeader>
+  );
+};
+
+export default Header;
