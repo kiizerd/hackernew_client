a959eb3e99ce99512260fa064f991280f827a8cb
kiizerd <ajsewell13@gmail.com>
2022-11-04 13:58:40 -0500

    Rewrote file to use single export and added .env variable to toggle log statements.

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,23 +1,25 @@
 const base = "https://hacker-news.firebaseio.com/v0/";
 const suffix = ".json?print=pretty";
 
-export const maxItemEndpoint = base + "maxitem" + suffix;
-export const newStoriesEndpoint = base + "newstories" + suffix;
-export const topStoriesEndpoint = base + "topstories" + suffix;
-export const bestStoriesEndpoint = base + "beststories" + suffix;
-export const jobStoriesEndpoint = base + "jobstories" + suffix;
-export const askStoriesEndpoint = base + "askstories" + suffix;
-export const showStoriesEndpoint = base + "showstories" + suffix;
-export const itemEndpoint = (itemNum) => base + `item/${itemNum}` + suffix;
-export const userEndpoint = (username) => base + `user/${username}` + suffix;
+const logRequests = import.meta.env.VITE_LOG_API_REQUESTS === 'true';
 
-export const itemEndpoints = {
+const maxItemEndpoint = base + "maxitem" + suffix;
+const newStoriesEndpoint = base + "newstories" + suffix;
+const topStoriesEndpoint = base + "topstories" + suffix;
+const bestStoriesEndpoint = base + "beststories" + suffix;
+const jobStoriesEndpoint = base + "jobstories" + suffix;
+const askStoriesEndpoint = base + "askstories" + suffix;
+const showStoriesEndpoint = base + "showstories" + suffix;
+const itemEndpoint = (itemNum) => base + `item/${itemNum}` + suffix;
+const userEndpoint = (username) => base + `user/${username}` + suffix;
+
+const itemEndpoints = {
   maxItem: maxItemEndpoint,
   item: itemEndpoint,
   user: userEndpoint,
 };
 
-export const collectionEndpoints = {
+const collectionEndpoints = {
   new: newStoriesEndpoint,
   top: topStoriesEndpoint,
   best: bestStoriesEndpoint,
@@ -26,31 +28,42 @@ export const collectionEndpoints = {
   job: jobStoriesEndpoint,
 };
 
-export const endpoints = {
+const endpoints = {
   ...itemEndpoints,
   ...collectionEndpoints,
 };
 
-export async function getRawCollection(endpointKey) {
+const getRawCollection = async (endpointKey) => {
   const endpoint = endpoints[endpointKey];
 
   return fetch(endpoint);
-}
+};
 
-export async function getCollection(endpointKey) {
+const getCollection = async (endpointKey) => {
   const endpoint = endpoints[endpointKey];
   const response = await fetch(endpoint);
   const collectionIds = await response.json();
-  console.log("getting collection", endpointKey);
+  logRequests && console.log("API - getting collection", endpointKey);
 
   return collectionIds;
-}
+};
 
-export async function getItem(itemId) {
+const getItem = async (itemId) => {
   const endpoint = itemEndpoint(itemId);
   const response = await fetch(endpoint);
   const data = await response.json();
-  console.log("getting item", itemId, data);
+  logRequests && console.log("API - getting item", itemId, data);
 
   return data;
-}
+};
+
+export {
+  getItem,
+  getCollection,
+  getRawCollection,
+  endpoints,
+  collectionEndpoints,
+  itemEndpoints,
+  itemEndpoint,
+  userEndpoint,
+};
