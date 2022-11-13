05e2e4f1b5e05d8100049d9dd49cd7bcb188bd5d
kiizerd <ajsewell13@gmail.com>
2022-11-13 00:08:22 -0600

    Rewrote <ItemList /> for useCollection hook.

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,8 +1,6 @@
-import { useEffect } from "react";
-import { useState } from "react";
 import styled from "styled-components";
 import ListItem from "./Item/ListItem";
-import PageControls from "./PageControls";
+import useCollection from "../hooks/useCollection";
 
 const List = styled.ul`
   border-top: 1px solid var(--primary-accent-color);
@@ -11,45 +9,21 @@ const List = styled.ul`
   padding: 2px 0;
 `;
 
-const ItemList = ({ collection, perPage }) => {
-  const [currentPage, setCurrentPage] = useState(0);
+const ItemList = ({ collection }) => {
+  const { currentPage, getChildren, CollectionControls } =
+    useCollection(collection);
 
-  const currentItemIds = (page) => {
-    const startIndex = page * perPage;
-    const endIndex = (page + 1) * perPage;
-
-    return collection.slice(startIndex, endIndex);
-  };
-
-  const currentItems = (page) => {
-    const ids = currentItemIds(page);
-
-    return ids.map((id) => (
-      <ListItem key={id} id={id} index={collection.indexOf(id)} />
-    ));
-  };
-
-  useEffect(() => {
-    setCurrentPage(0);
-  }, [collection])
-
-  const incrementPage = () => setCurrentPage(currentPage + 1);
-  const decrementPage = () => setCurrentPage(currentPage - 1);
-  const controls = (
-    <PageControls
-      increment={incrementPage}
-      decrement={decrementPage}
-      current={currentPage}
-    />
+  const CollectionElement = ({ id }) => (
+    <ListItem key={id} id={id} index={collection.indexOf(id)} />
   );
 
   return (
-    <section>
-      {controls}
+    <section style={{ position: "relative" }}>
+      <CollectionControls />
       {/* Key prop is necessary to re-render list element when currentPage changes */}
       {/* DO NOT REMOVE -- TRY TO REMEMBER */}
-      <List key={currentPage}> {currentItems(currentPage)} </List>
-      {controls}
+      <List key={currentPage}> {getChildren(CollectionElement)} </List>
+      <CollectionControls />
     </section>
   );
 };
