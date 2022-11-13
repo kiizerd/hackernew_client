59a5f06eae2b2ff169b5af49a11c6d719200bfeb
kiizerd <ajsewell13@gmail.com>
2022-11-12 23:51:00 -0600

    Condensed previously problematic, unnecessary hook into useForagedData custom hook.

--- src/hooks/useForagedData.jsx
+++ src/hooks/useForagedData.jsx
@@ -1,17 +1,45 @@
 import { useEffect, useState } from "react";
 import { forageItem } from "../api/storage";
+import ReactTimeAgo from "react-time-ago";
 
 export default (itemId) => {
   const [data, setData] = useState({});
+  const [link, setLink] = useState("");
+  const [time, setTime] = useState(false);
+  const localURL = `/item/${itemId}`;
 
   useEffect(() => {
     const getItemData = async () => {
       const item = await forageItem(`${itemId}`);
-      setData({ ...item, localURL: `/item/${itemId}` });
+      setData({
+        ...item,
+        localURL,
+      });
     };
 
     getItemData();
   }, []);
 
-  return data;
+  useEffect(() => {
+    const getItemTime = () => {
+      if (data.time && !time) {
+        const timestamp = data.time * 1000;
+        setTime(<ReactTimeAgo date={timestamp} />);
+      }
+    };
+
+    const getItemLink = () => {
+      if (data.url) {
+        const itemURL = new URL(data.url);
+        setLink(itemURL);
+      } else {
+        setLink(localURL);
+      }
+    };
+
+    getItemLink();
+    getItemTime();
+  }, [data]);
+
+  return { ...data, link, time };
 };
