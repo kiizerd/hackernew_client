1c4e781a8cc235a1bb6064dd4b450abc66cae78c
kiizerd <ajsewell13@gmail.com>
2022-11-12 23:47:13 -0600

    Wrote custom hook to manage collections retrieved from API.

---
+++ src/hooks/useCollection.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import PageControls from "../components/PageControls";
+import PerPageSelector from "../components/PerPageSelector";
+import calcMaxPage from "../helpers/calcMaxPage";
+
+export default (collection) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [perPage, setPerPage] = useState(16);
+  const [maxPage, setMaxPage] = useState(0);
+
+  useEffect(() => {
+    setCurrentPage(0);
+  }, [collection]);
+
+  useEffect(() => {
+    const len = collection.length;
+    const newMax = calcMaxPage(len, perPage);
+    setMaxPage(newMax);
+
+    if (currentPage > newMax) setCurrentPage(newMax);
+  }, [perPage]);
+
+  const getSubCollection = (page) => {
+    const startIndex = page * perPage;
+    const endIndex = (page + 1) * perPage;
+
+    return collection.slice(startIndex, endIndex);
+  };
+
+  const getChildren = (CollectionElement) => {
+    const subCollection = getSubCollection(currentPage);
+
+    return subCollection.map((id) =>(
+      <CollectionElement key={id} id={id} index={collection.indexOf(id)} />
+    ));
+  };
+
+  const CollectionControls = () => (
+    <>
+      <PageControls
+        current={currentPage}
+        max={maxPage}
+        update={setCurrentPage}
+      />
+      <PerPageSelector current={perPage} update={setPerPage} />
+    </>
+  );
+
+  return { currentPage, getChildren, CollectionControls };
+};
